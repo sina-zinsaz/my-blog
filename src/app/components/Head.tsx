3d53bb260af1df9564d4a34234d94281ad06f98d
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-12-22 14:42:19 +0330

    add page join us

--- src/app/components/Head.tsx
+++ src/app/components/Head.tsx
@@ -1,5 +1,14 @@
-import { Box, Link } from "@mui/material";
+"use client";
+import { Box, Link, keyframes } from "@mui/material";
 import { socialLinks } from "../constants/socialLinks";
+
+const colorChange = keyframes`
+  0% { border-color: red; }
+  25% { border-color: blue; }
+  50% { border-color: green; }
+  75% { border-color: orange; }
+  100% { border-color: red; }
+`;
 
 export default function Head() {
   return (
@@ -10,12 +19,27 @@ export default function Head() {
         alignItems: "center",
       }}
     >
-      <Box
-        component="img"
-        src="assets/images/profile.jpg"
-        alt="Profile"
-        sx={{ width: { xs: "15%", md: "5%" }, borderRadius: "50%" }}
-      />
+      <Box display="flex" alignItems="center">
+        <Box
+          component="img"
+          src="assets/images/profile.jpg"
+          alt="Profile"
+          sx={{ width: { xs: "15%", md: "10%" }, borderRadius: "50%" }}
+        />
+        <Link
+          href="#"
+          sx={{
+            textDecoration: "none",
+            ml: 2,
+            border: "2px solid",
+            padding: "4px 12px",
+            borderRadius: "8px",
+            animation: `${colorChange} 4s infinite`,
+          }}
+        >
+          Join Us
+        </Link>
+      </Box>
       <Box sx={{ display: "flex", gap: 2 }}>
         {socialLinks.map(({ href, icon: Icon }, index) => (
           <Link
---
+++ src/app/join-us/layout.tsx
@@ -0,0 +1,13 @@
+export default function JoinUsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
