1d032f5a4bb9674491a7a4b5d10a631c504496e2
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-12-23 13:49:15 +0330

    new update scrollbar for light and dark mode

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -12,6 +12,26 @@ export const lightTheme: Theme = createTheme({
       paper: '#f5f5f5',
     },
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "&::-webkit-scrollbar": {
+            width: "10px",
+            height: "10px",
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "#f0f0f0",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#f0f0f0",
+            borderRadius: "5px",
+            border: "1px solid rgb(0, 0, 0)",
+          },
+        },
+      },
+    },
+  },
 });
 
 export const darkTheme: Theme = createTheme({
@@ -25,4 +45,25 @@ export const darkTheme: Theme = createTheme({
       paper: '#121212',
     },
   },
+    components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "&::-webkit-scrollbar": {
+            width: "10px",
+            height: "10px",
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "#0a0a0a",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#14191e",
+            borderRadius: "5px",
+            border: "1px solid #f4f4f4",
+          },
+        },
+      },
+    },
+  },
 });
+
