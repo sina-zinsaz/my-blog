a5a10401fe4bd0422ee820f33616231cf21e19a2
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-12-27 01:28:15 +0330

    update join us page

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+"use client";
+import { Box, Typography } from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  return <div>page</div>;
+  const router = useRouter();
+
+  return (
+    <Box>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <HomeRoundedIcon
+          onClick={() => router.push("/")}
+          sx={{ cursor: "pointer" }}
+          fontSize="large"
+        />
+        <Typography variant="h5">Sina Zinsaz</Typography>
+      </Box>
+    </Box>
+  );
 }
